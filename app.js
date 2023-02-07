462d0f71eb6791dd845dc3da4a6c09551ea22272
Ovee Hasnat <abu.hasnat@northsouth.edu>
2023-02-08 01:52:45 +0600

    Template views & Routing added

---
+++ app.js
@@ -0,0 +1,55 @@
+//external dependencies
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+// internal imports
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middlewares/common/errorHandler");
+const loginRouter = require("./routers/loginRouter");
+const usersRouter = require("./routers/usersRouter");
+const inboxRouter = require("./routers/inboxRouter");
+
+const app = express();
+dotenv.config();
+
+// database connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database connection successful!"))
+  .catch((err) => console.log(err));
+
+// request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folders
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
+
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});
---
+++ routers/usersRouter.js
@@ -0,0 +1,13 @@
+// external import
+const express = require("express");
+
+// internal imports
+const { getUsers } = require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+
+const router = express.Router();
+
+// Users page
+router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+module.exports = router;
